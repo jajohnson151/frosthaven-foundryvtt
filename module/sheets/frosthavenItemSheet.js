41758976ba92801610d5cc31049fc23c2fd944e0
AJ Johnson <phreneticdragon@gmail.com>
2022-12-04 14:25:44 -0500

    AJ: Last push before re-starting project

--- module/sheets/frosthavenItemSheet.js
+++ module/sheets/frosthavenItemSheet.js
@@ -1,16 +1,28 @@
-export default class frosthavenItemSheet extends ItemSheet {
+export class frosthavenItemSheet extends ItemSheet {
     // template - return path to an HTML file
     get template() {
         //alert("A messge from Scoobs");
-        console.log(this);
+        console.log('frosthavenItemSheet.template(): ENTER');
+        //console.log(this);
         return 'systems/frosthaven/module/sheets/' + 'basicItem' + '-sheet.html';
     }
 
-    getData() {
-        const data = super.getData();
+    async getData() {
+        console.log('frosthavenItemSheet.getData(): ENTER');
+        const context = super.getData();
+        //console.log(this);
 
-        data.config = CONFIG.frosthaven;
+        //context.config = CONFIG.frosthaven;
 
-        return data;
+        // Use a safe clone of the item data for further operations.
+        const itemData = context.item;
+
+       // Add the actor's data to context.data for easier access, as well as flags.
+        context.data = itemData.system;
+        context.flags = itemData.flags;
+
+        console.log('frosthavenItemSheet.getData(): EXIT');
+
+        return context;
     }
 }
